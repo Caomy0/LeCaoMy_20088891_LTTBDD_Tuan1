6e46f7fa096589e7775e628101446a5f29518542
Caomy0 <mynho1132002@gmail.com>
2024-10-03 23:57:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PrintNumbers from './BaiTap/Cau1';
+import NumberDisplay from './BaiTap/Cau1';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberDisplay/>
+
+    </div>
+  );
+}
+
+export default App;
